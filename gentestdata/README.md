b290dbf42cae8ed8e57461c788622c159351aec0
YAMASAKI Masahide <masahide.y@gmail.com>
2021-10-12 10:59:13 +0900

    add gentestdata

---
+++ gentestdata/README.md
@@ -0,0 +1,7 @@
+gentestdata
+============
+
+
+```bash
+ go run main.go | mysql -h 127.0.0.1 -P 3330 -u 'root:secret@127.0.0.1:3306'
+```
---
+++ gentestdata/main.go
@@ -0,0 +1,27 @@
+package main
+
+import (
+	"bytes"
+	"flag"
+	"os"
+)
+
+var (
+	loop = 100000
+	size = 23 * 15
+)
+
+func main() {
+
+	flag.IntVar(&loop, "l", loop, "loop count")
+	flag.IntVar(&size, "s", size, "query text size")
+	flag.Parse()
+
+	for i := 0; i < loop; i++ {
+		os.Stdout.Write([]byte("select CHAR_LENGTH(\""))
+		//os.Stdout.Write(bytes.Repeat([]byte("a"), 23*16)) // 10 * 16 NG
+		os.Stdout.Write(bytes.Repeat([]byte("a"), 23*15)) // 10 * 15 OK
+		os.Stdout.Write([]byte("\") as len;\n"))
+
+	}
+}
