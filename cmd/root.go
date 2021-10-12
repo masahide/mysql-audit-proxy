14aca065e12e50395bc1eb04a9d10a58b1d79dfe
YAMASAKI Masahide <masahide.y@gmail.com>
2021-10-12 22:56:36 +0900

    fix

--- cmd/root.go
+++ cmd/root.go
@@ -40,7 +40,7 @@ var (
 		RotateTime:      1 * time.Hour,
 		QueueSize:       200,
 		BufSize:         "32mb",
-		EncodeType:      mysql.EncodeTypeGOB,
+		EncodeType:      mysql.EncodeTypeGencode,
 		BufferFlushTime: 1 * time.Second,
 	}
 )
